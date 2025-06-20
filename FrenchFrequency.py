8806adeac839c7d7726bb4049e14ad06713dfd5e
olive11224 <159417566+olive11224@users.noreply.github.com>
2025-06-19 23:59:23 -0400

    Update FrenchFrequency.py

--- FrenchFrequency.py
+++ FrenchFrequency.py
@@ -17,36 +17,30 @@ session.mount('https://', adapter)
 URL = 'https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/French_wordlist_opensubtitles_5000'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find_all('span', class_ = 'Latn')
+results = soup.find_all('span', class_ = 'Latn') #Pulls all words
 
 result_content = ''
 
 for result in results:
-    result_content =result_content + result.text + '\n'
+    result_content =result_content + result.text + '\n' #All the words in a string separated by \n
 
-with open('C:/Users/Computer/Downloads/Python Scripts/FrenchWordsBeforeLemmatization.txt', 'w') as f:
-    f.write(result_content)
-
-resultLemmaobj = nlp(result_content)
+resultLemmaobj = nlp(result_content) #Prepares string for lemmatization 
 
 resultLemma = ''
 for token in resultLemmaobj:
     lemma = spell.correction(token.lemma_)
     if type(lemma) == str:
-        resultLemma = resultLemma + lemma + '\n'
+        resultLemma = resultLemma + lemma + '\n' #Lemmatizes the words and saves them into a string separated by \n
 
-with open('C:/Users/Computer/Downloads/Python Scripts/FrenchWordsAfterLemmatization.txt', 'w') as f:
-    f.write(resultLemma)
-
-words = resultLemma.split()
-resultLemmaUnduplicate = " ".join(sorted(set(words), key=words.index))
-resultLemmaUnduplicate1 = resultLemmaUnduplicate.split()
+words = resultLemma.split() #Splits the string into a list based on \n
+resultLemmaUnduplicate = " ".join(sorted(set(words), key=words.index)) #Deletes duplicate words. key=words.index ensures that the original ordering is preserved.
+resultLemmaUnduplicate1 = resultLemmaUnduplicate.split() #Re-lists the words, separated by spaces
 resultLemmaUnduplicate = ''
 for token in resultLemmaUnduplicate1:
-    resultLemmaUnduplicate = resultLemmaUnduplicate + token + '\n'
+    resultLemmaUnduplicate = resultLemmaUnduplicate + token + '\n' #Converts into a string
 
-with open('C:/Users/Computer/Downloads/Python Scripts/FrenchWordsAfterFilter.txt', 'w') as f:
-    f.write(resultLemmaUnduplicate)
+with open('', 'w') as f: #Insert your desired path
+    f.write(resultLemmaUnduplicate) #Writes to text file
 
 
 
